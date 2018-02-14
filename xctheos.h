b4a456cfbb7357f98632b477028d80c10584f7c0
onezens <leafnet02@qq.com>
2018-02-14 20:25:34 +0800

    update new

--- xctheos.h
+++ xctheos.h
@@ -4,7 +4,7 @@
 //
 //  Created by Adam Bell on 2014-03-24.
 //  Copyright (c) 2014 Adam Bell. All rights reserved.
-//
+// http://iphonedevwiki.net/index.php/Logos
 
 #ifdef __OBJC__
     #define _STRING(stuff) #stuff
@@ -14,7 +14,29 @@
     #define CONCAT(stuff, otherstuff) _CONCAT(stuff, otherstuff)
     #define UNIQUE_SUFFIX(stuff) CONCAT(stuff, __COUNTER__)
 
-    #ifdef __LOGOS_H
+    #ifdef XcodeTheos
+        #import <objc/runtime.h>
+
+        #define STFU_CLANG _Pragma("clang diagnostic push") _Pragma("clang diagnostic ignored \"-Wobjc-protocol-method-implementation\"")
+        #define END_STFU_CLANG
+        #define CONFIG(stuff)
+        #define INIT(stuff)
+        #define GROUP(stuff)
+        #define END_GROUP()
+        #define HOOK(stuff) STFU_CLANG @interface stuff(UNIQUE_SUFFIX(hax)) @end @implementation stuff (UNIQUE_SUFFIX(hax))
+        #define HOOK_AND_DECLARE(stuff, superclass) STFU_CLANG @interface stuff : superclass @end @implementation stuff
+        #define NEW(...)
+        #define END() @end END_STFU_CLANG
+        #define ORIG(...) nil
+        #define ORIG_T(...) 0
+        #define GET_CLASS(stuff) objc_getClass(STRING(stuff))
+        #define SUBCLASS(stuff, superclass) @interface stuff : superclass @end @implementation stuff
+        #define END_SUBCLASS() @end
+        #define CTOR() static void inline stuff()
+        #define NEWPROPERTY(type, strong, name)
+        #define NEWPROPERTY_T(type, assign, name)
+        #define NEWPROPERTY_Block(type, name)
+    #else
         #define CONFIG(stuff) %config(stuff)
         #define INIT(stuff) %init(stuff)
         #define GROUP(stuff) %group stuff
@@ -28,26 +50,11 @@
         #define GET_CLASS(stuff) %c(stuff)
         #define SUBCLASS(stuff, superclass) %subclass stuff : superclass
         #define END_SUBCLASS() %end
-        #define CTOR(hax) %ctor hax
-    #else
-        #import <objc/runtime.h>
+        #define CTOR() %ctor
+        #define NEWPROPERTY(type, strong, name) %property (nonatomic,strong) type * name
+        #define NEWPROPERTY_T(type, assign, name) %property (nonatomic,assign) type name
+        #define NEWPROPERTY_Block(type, name) %property (nonatomic,copy) type name
 
-        #define CLANG_NO_U _Pragma("clang diagnostic push") _Pragma("clang diagnostic ignored \"-Wobjc-protocol-method-implementation\"") _Pragma("clang diagnostic ignored \"-Wunused-function\"")
-        #define CLANG_NO_U_END _Pragma("clang diagnostic pop") _Pragma("clang diagnostic pop")
-
-        #define CONFIG(stuff)
-        #define INIT(stuff)
-        #define GROUP(stuff)
-        #define END_GROUP()
-        #define HOOK(stuff) CLANG_NO_U @interface stuff(UNIQUE_SUFFIX(hax)) @end @implementation stuff (UNIQUE_SUFFIX(hax))
-        #define HOOK_AND_DECLARE(stuff, superclass) CLANG_NO_U @interface stuff : superclass @end @implementation stuff
-        #define NEW(...)
-        #define END() @end CLANG_NO_U_END
-        #define ORIG(...) nil
-        #define ORIG_T(...) 0
-        #define GET_CLASS(stuff) objc_getClass(STRING(stuff))
-        #define SUBCLASS(stuff, superclass) @interface stuff : superclass @end @implementation stuff
-        #define END_SUBCLASS() @end
-        #define CTOR(hax) CLANG_NO_U static void inline stuff() hax CLANG_NO_U_END
     #endif
 #endif
+
